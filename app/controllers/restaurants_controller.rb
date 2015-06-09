30a474073f0d1efc804903976b721c693042d1f2
trish3677 <gleason.trish@gmail.com>
2015-06-08 22:08:56 -0500

    final commit

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,7 +2,7 @@
  class RestaurantsController < ApplicationController
 
   def index
-      @restaurants = Restaurant.order('name asc').page(params[:page]).per(3)
+      @restaurants = Restaurant.order('name asc').page(params[:page]).per(8)
   end
 
   def show
